aaa63760dd5706a88c13098e0d28cfdd09ed0054
Angelo Porretta <angelo.porretta@utoronto.ca>
2020-09-25 08:26:15 -0400

    Here we go again.

--- .platform/hooks/prebuild/server_software.sh
+++ .platform/hooks/prebuild/server_software.sh
@@ -3,15 +3,15 @@ yum -y update
 # Installing software.
 
 # Mysql
-# mysqltest=`yum list installed bind`
-# if [ "$mysqltest"="Error: No matching Packages to list" ]; then
-#   yum install -y mysql
-# fi
+mysqltest=`yum list installed bind`
+if [ "$mysqltest"="Error: No matching Packages to list" ]; then
+  yum install -y mysql
+fi
 
 #Supervisor
-# easy_install supervisor
-# if [ ! -d "/etc/supervisor" ]; then
-#   mkdir /etc/supervisor
-# fi
-# cp /var/app/staging/.platform/hooks/prebuild/copy_files/supervisor.conf /etc/supervisor/supervisor.conf
-# cp /var/app/staging/.platform/hooks/prebuild/copy_files/supervisord /usr/bin/supervisord
+easy_install supervisor
+if [ ! -d "/etc/supervisor" ]; then
+  mkdir /etc/supervisor
+fi
+cp /var/app/staging/.platform/hooks/prebuild/copy_files/supervisor.conf /etc/supervisor/supervisor.conf
+cp /var/app/staging/.platform/hooks/prebuild/copy_files/supervisord /usr/bin/supervisord
