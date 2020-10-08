eaae740ff8a0c2211695e17e1edf12abb4581fb9
Angelo Porretta <angelo.porretta@utoronto.ca>
2020-10-07 23:23:12 -0400

    Attempt at fixing missing venv folder.

--- appspec/after_install.sh
+++ appspec/after_install.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-# . /var/app/venv/bin/activate
-# pip install -r /var/app/current/requirements.txt --user
+ENVDIR="/var/app/venv/"
+if [ ! -d "$ENVDIR" ]; then
+  mkdir /var/app
+  mkdir /var/app/venv
+  python3 -m venv /var/app/venv
+fi
+. /var/app/venv/bin/activate
+pip install -r /var/app/current/requirements.txt --user
