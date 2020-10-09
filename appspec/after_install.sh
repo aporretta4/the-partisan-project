2269a156ec87e13ad38552ada307e652301bbcfe
Angelo Porretta <angelo.porretta@utoronto.ca>
2020-10-09 08:35:55 -0400

    Change the ownership of the venv.

--- appspec/after_install.sh
+++ appspec/after_install.sh
@@ -2,6 +2,7 @@
 ENVDIR="/home/ec2-user/venv/"
 if [ ! -d "$ENVDIR" ]; then
   python3 -m venv /home/ec2-user/venv
+  chown -R ec2-user /home/ec2-user/venv
 fi
 . /home/ec2-user/venv/bin/activate
 pip install -r /var/app/current/requirements.txt --user
