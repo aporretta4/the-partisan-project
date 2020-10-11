c93cdd5088489ac6602a39b8da5260600f4997b5
Angelo Porretta <angelo.porretta@utoronto.ca>
2020-10-10 23:09:47 -0400

    Big discovery. Just figured out we no longer need nginx or supervisor.

--- appspec/after_install.sh
+++ appspec/after_install.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 ENVDIR="/home/ec2-user/venv/"
+pkill gunicorn
 if [ ! -d "$ENVDIR" ]; then
   python3 -m venv /home/ec2-user/venv
   chown -R ec2-user /home/ec2-user/venv
 fi
 . /home/ec2-user/venv/bin/activate
 pip3 install -r /var/app/current/requirements.txt
+cd /var/app/current
+/home/ec2-user/venv/bin/gunicorn --bind 127.0.0.1:8000 django_project.wsgi --daemon
