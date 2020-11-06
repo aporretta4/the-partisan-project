76654f809a6af2588b1bf843e4abec7212bec77f
Angelo Porretta <angelo.porretta@utoronto.ca>
2020-11-06 15:30:59 -0500

    Adding sentiment columns to model.

--- partisan/models.py
+++ partisan/models.py
@@ -7,7 +7,11 @@ class tweet(models.Model):
     text = models.CharField(max_length=2048, )
     author_id = models.BigIntegerField(editable=False)
     created_at = models.DateTimeField(default='1970-01-01 00:00:00+00:00')
-    google_nlp_processed = models.BooleanField(default=False)
+    nlp_processed = models.BooleanField(default=False)
+    nlp_neutral_sentiment = models.DecimalField(max_digits=30,decimal_places=20,null=True)
+    nlp_positive_sentiment = models.DecimalField(max_digits=30,decimal_places=20,null=True)
+    nlp_negative_sentiment = models.DecimalField(max_digits=30,decimal_places=20,null=True)
+    nlp_mixed_sentiment = models.DecimalField(max_digits=30,decimal_places=20,null=True)
 
 class tw_retriever_metadata(models.Model):
     id = models.CharField(primary_key=True,editable=False,unique=True,max_length=255)
