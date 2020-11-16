38f8de58dea9d307bb3a82faf22d2a4d2abea887
Angelo Porretta <angelo.porretta@utoronto.ca>
2020-11-16 09:36:57 -0500

    Adding search term model.

--- partisan/models.py
+++ partisan/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class search_term(models.Model):
+    id = models.BigIntegerField(primary_key=True,editable=False,unique=True)
+    term = models.CharField(editable=False,unique=False,max_length=255)
 
 class tweet(models.Model):
     id = models.BigIntegerField(primary_key=True,editable=False,unique=True)
@@ -13,6 +15,7 @@ class tweet(models.Model):
     nlp_positive_sentiment = models.DecimalField(max_digits=30,decimal_places=20,null=True)
     nlp_negative_sentiment = models.DecimalField(max_digits=30,decimal_places=20,null=True)
     nlp_mixed_sentiment = models.DecimalField(max_digits=30,decimal_places=20,null=True)
+    term = models.ForeignKey(search_term,null=False,on_delete=models.RESTRICT)
 
 class tw_retriever_metadata(models.Model):
     id = models.CharField(primary_key=True,editable=False,unique=True,max_length=255)
