4915c572dcb013a3f68d323e56dc22b6b5f36c3f
Angelo Porretta <angelo.porretta@utoronto.ca>
2020-11-15 20:36:25 -0500

    Adding creation of static files.

--- appspec/after_install.sh
+++ appspec/after_install.sh
@@ -10,4 +10,6 @@ chmod 775 /home/ec2-user/venv/bin/activate
 pip3 install -r /var/app/current/requirements.txt
 cd /var/app/current
 python3 manage.py migrate
+rm -R django_project/static/*
+python3 manage.py collectstatic --noinput
 /home/ec2-user/venv/bin/gunicorn --bind 127.0.0.1:8000 django_project.wsgi --daemon
--- django_project/settings.py
+++ django_project/settings.py
@@ -131,10 +131,35 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
+
+
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/var/www/example.com/static/"
+STATIC_ROOT = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Additional locations of static files
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(BASE_DIR, "partisan/static"),
+)
+
+# List of finder classes that know how to find static files in
+# various locations.
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
+
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
