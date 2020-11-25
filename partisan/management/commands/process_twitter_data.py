7d0f063d96e631af43b280912a45254b83ebec59
Angelo Porretta <angelo.porretta@utoronto.ca>
2020-11-25 10:43:46 -0500

    Making comprehend command have a configurable batch size.

--- partisan/management/commands/process_twitter_data.py
+++ partisan/management/commands/process_twitter_data.py
@@ -4,6 +4,10 @@ from partisan.p_classes.processors.twitter import twitter_processor
 
 class Command(BaseCommand):
 
+    def add_arguments(self, parser):
+        parser.add_argument('--batchsize', type=int, help='The amount of records you want to run through AWS comprehend.', default=100, required=False)
+
+
     def handle(self, *args, **kwargs):
         processor = twitter_processor()
-        processor.processTweets()
+        processor.processTweets(process_number=kwargs['batchsize'])
