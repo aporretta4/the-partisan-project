3e30a40dcb11fcfc439b4ab791f2a02532e42219
Angelo Porretta <angelo.porretta@utoronto.ca>
2020-11-30 09:39:18 -0500

    Changing data population script to reflect aws environments.

--- appspec/pull_test_data.sh
+++ appspec/pull_test_data.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
-. /Users/angeloporretta/eb-virt/bin/activate
-cd /Users/angeloporretta/django_project
+ENVDIR="/home/ec2-user/venv/"
+if [ ! -d "$ENVDIR" ]; then
+  python3 -m venv /home/ec2-user/venv
+fi
+. /home/ec2-user/venv/bin/activate
+cd /var/app/current
 
 for i in 1 2 3 4 5
 do
-  python manage.py pull_twitter_data "election" --batchsize=500
-  python manage.py pull_twitter_data "us politics" --batchsize=500
-  python manage.py pull_twitter_data "uk politics" --batchsize=500
-  python manage.py pull_twitter_data "canadian politics" --batchsize=1500
-  python manage.py pull_twitter_data "covid" --batchsize=100
-  python manage.py process_twitter_data --batchsize=1000
+  python3 manage.py pull_twitter_data "election" --batchsize=500
+  python3 manage.py pull_twitter_data "us politics" --batchsize=500
+  python3 manage.py pull_twitter_data "uk politics" --batchsize=500
+  python3 manage.py pull_twitter_data "canadian politics" --batchsize=1500
+  python3 manage.py pull_twitter_data "covid" --batchsize=100
+  python3 manage.py process_twitter_data --batchsize=1000
   # Process the stats
-  python manage.py process_sentiment_pie_stats "election"
-  python manage.py process_sentiment_pie_stats "covid"
-  python manage.py process_sentiment_pie_stats "uk politics"
-  python manage.py process_sentiment_pie_stats "us politics"
-  python manage.py process_sentiment_pie_stats "canadian politics"
+  python3 manage.py process_sentiment_pie_stats "election"
+  python3 manage.py process_sentiment_pie_stats "covid"
+  python3 manage.py process_sentiment_pie_stats "uk politics"
+  python3 manage.py process_sentiment_pie_stats "us politics"
+  python3 manage.py process_sentiment_pie_stats "canadian politics"
 done
