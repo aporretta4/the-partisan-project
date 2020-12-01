a7d33ed861553ff6cd1b5f418be7705467fed9aa
Angelo Porretta <angelo.porretta@utoronto.ca>
2020-12-01 11:10:20 -0500

    Going into proper directory of appspec and removing the need for collecting static because we're now using Cloudfront.

--- appspec/after_install.sh
+++ appspec/after_install.sh
@@ -8,8 +8,7 @@ chown -R ec2-user /home/ec2-user/venv
 chmod 775 /home/ec2-user/venv/bin/activate
 . /home/ec2-user/venv/bin/activate
 pip3 install -r /var/app/current/requirements.txt
+cd /var/app/current
 python3 manage.py migrate
-rm -R django_project/static/*
-python3 manage.py collectstatic --noinput
 aws s3 sync "partisan/static/" "s3://$STATIC_FILE_BUCKET_NAME"
 /home/ec2-user/venv/bin/gunicorn --bind 127.0.0.1:8000 django_project.wsgi --daemon
