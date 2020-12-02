368035ce39f416faba9f4a4d0522cc81b1a8b0c7
Angelo Porretta <angelo.porretta@utoronto.ca>
2020-12-02 08:30:31 -0500

    Tempering auto pull.

--- appspec/pull_test_data.sh
+++ appspec/pull_test_data.sh
@@ -8,12 +8,12 @@ cd /var/app/current
 
 for i in 1 2 3 4 5
 do
-  python3 manage.py pull_twitter_data "election" --batchsize=500
-  python3 manage.py pull_twitter_data "us politics" --batchsize=500
-  python3 manage.py pull_twitter_data "uk politics" --batchsize=500
+  python3 manage.py pull_twitter_data "election" --batchsize=100
+  python3 manage.py pull_twitter_data "us politics" --batchsize=100
+  python3 manage.py pull_twitter_data "uk politics" --batchsize=100
   python3 manage.py pull_twitter_data "canadian politics" --batchsize=1500
   python3 manage.py pull_twitter_data "covid" --batchsize=100
-  python3 manage.py process_twitter_data --batchsize=1000
+  python3 manage.py process_twitter_data --batchsize=2000
   # Process the stats
   python3 manage.py process_sentiment_pie_stats "election"
   python3 manage.py process_sentiment_pie_stats "covid"
