3daef7019bcbb2054d280e966a2f397bacdcbe6e
Angelo Porretta <angelo.porretta@utoronto.ca>
2020-12-03 16:16:38 -0500

    Code refactor to make aws comprehend calls more DRY.

--- partisan/management/commands/process_twitter_data.py
+++ partisan/management/commands/process_twitter_data.py
@@ -1,13 +1,12 @@
-from django.core.management.base import BaseCommand, CommandError
-from partisan.p_classes.nlp.aws_comprehend import comprehender
-from partisan.p_classes.processors.twitter import twitter_processor
+from django.core.management.base import BaseCommand
+from partisan.models import tweet
+from partisan.p_classes.nlp.sentimentable import sentimentable
 
 class Command(BaseCommand):
 
-    def add_arguments(self, parser):
-        parser.add_argument('--batchsize', type=int, help='The amount of records you want to run through AWS comprehend.', default=100, required=False)
+  def add_arguments(self, parser):
+    parser.add_argument('--batchsize', type=int, help='The amount of records you want to run through AWS comprehend.', default=100, required=False)
 
-
-    def handle(self, *args, **kwargs):
-        processor = twitter_processor()
-        processor.processTweets(process_number=kwargs['batchsize'])
+  def handle(self, *args, **kwargs):
+    tweets = tweet.objects.filter(nlp_processed=False, ).order_by('id')[:kwargs['batchsize']]
+    sentimentable.saveTextSentiment(sentimentable_classes=tweets)
---
+++ partisan/p_classes/nlp/sentimentable.py
@@ -0,0 +1,42 @@
+import logging
+from django.db import models
+from partisan.p_classes.nlp.aws_comprehend import comprehender
+
+class sentimentable():
+
+  @staticmethod
+  def saveTextSentiment(sentimentable_classes: models.QuerySet):
+    sentiment_comprehender = comprehender()
+    sentimentable_list = list(sentimentable_classes)
+    text_list = [None] * sentimentable_classes.count()
+    processed_sentiment_list = []
+    processed_index = 0
+    for i in range(sentimentable_classes.count()):
+      text_list[i] = sentimentable_list[i].text
+    text_list = list(sentimentable.__chunk(text_list=text_list, n=25))
+    try:
+      for i in range(len(text_list)):
+        sentiment_list = sentiment_comprehender.comprehendText(text=text_list[i])
+        for j in range(len(sentiment_list['ResultList'])):
+          processed_sentiment_list.append(sentiment_list['ResultList'][j])
+      for i in range(sentimentable_classes.count()):
+        sentimentable_classes[i].nlp_processed = True
+        if processed_sentiment_list[i]['SentimentScore']['Mixed'] < 0.25:
+          sentimentable_list[i].nlp_neutral_sentiment = processed_sentiment_list[i]['SentimentScore']['Neutral']
+          sentimentable_list[i].nlp_mixed_sentiment = processed_sentiment_list[i]['SentimentScore']['Mixed']
+          sentimentable_list[i].nlp_negative_sentiment = processed_sentiment_list[i]['SentimentScore']['Negative']
+          sentimentable_list[i].nlp_positive_sentiment = processed_sentiment_list[i]['SentimentScore']['Positive']
+        if sentimentable_list[i].nlp_neutral_sentiment == None or sentimentable_list[i].nlp_mixed_sentiment == None or sentimentable_list[i].nlp_negative_sentiment == None or sentimentable_list[i].nlp_positive_sentiment == None:
+          sentimentable_list[i].delete()
+        else:
+          sentimentable_list[i].save()
+        processed_index += 1
+    except Exception as ex:
+      logging.error(str(ex))
+      print(str(ex))
+      sentimentable_classes[processed_index].delete()
+
+  @staticmethod
+  def __chunk(text_list: list, n: int):
+    for i in range(0, len(text_list), n):
+      yield text_list[i:i + n]
