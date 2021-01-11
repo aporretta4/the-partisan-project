ac68f71b9ee15bb9c202b3e23f2a6a834f1db8e2
Angelo Porretta <angelo.porretta@utoronto.ca>
2021-01-11 16:57:34 -0500

    Static file versioning in order to bust caches, including S3 and CloudFront caches.

--- partisan/gulpfile.js
+++ partisan/gulpfile.js
@@ -12,8 +12,64 @@ let plumber = require('gulp-plumber');
 let minify = require('gulp-minify');
 let concat = require('gulp-concat');
 let clean = require('gulp-clean');
+let CacheBuster = require('gulp-cachebust');
 
-gulp.task('buildSass', () => {
+let buster = new CacheBuster({
+  'pathFormatter': (dirname, basename, extname, checksum) => {
+    return require('path').join(dirname, basename + '___' + checksum + extname);
+  }
+});
+
+gulp.task('gatherFileHashes', (done) => {
+    return gulp.src('frontend/**/*.*').pipe(buster.resources());
+});
+
+gulp.task('removeNodePackageJs', (done) => {
+  if (fs.existsSync('static/node_modules')) {
+    return gulp.src('static/node_modules').pipe(clean());
+  }
+  else {
+    done();
+  }
+});
+
+gulp.task('removeJs', (done) => {
+  if (fs.existsSync('static/js')) {
+    return gulp.src('static/js').pipe(clean());
+  }
+  else {
+    done();
+  }
+});
+
+gulp.task('removeCss', (done) => {
+  if (fs.existsSync('static/css')) {
+    return gulp.src(['static/css']).pipe(clean());
+  }
+  else {
+    done();
+  }
+});
+
+gulp.task('removeFonts', (done) => {
+  if (fs.existsSync('static/fonts')) {
+    return gulp.src(['static/fonts']).pipe(clean());
+  }
+  else {
+    done();
+  }
+});
+
+gulp.task('removeImgs', (done) => {
+  if (fs.existsSync('static/img')) {
+    return gulp.src(['static/img']).pipe(clean());
+  }
+  else {
+    done();
+  }
+});
+
+gulp.task('createSass', () => {
   return gulp.src('frontend/sass/**/*.scss')
     .pipe(plumber({
       errorHandler: (err) => {
@@ -27,32 +83,17 @@ gulp.task('buildSass', () => {
     .pipe(sass())
     .pipe(autoprefixer())
     .pipe(source.write('maps'))
+    .pipe(buster.resources())
+    .pipe(buster.references())
     .pipe(gulp.dest('static/css'));
 });
 
-gulp.task('removeNodePackageJs', (done) => {
-  if (fs.existsSync('static/node_modules')) {
-    return gulp.src('static/node_modules').pipe(clean());
-  }
-  else {
-    done();
-  }
-});
-
-gulp.task('removeJs', (done) => {
-  if (fs.existsSync('static/js/scripts') && fs.existsSync('static/js/libraries')) {
-    return gulp.src(['static/js/scripts', 'static/js/libraries']).pipe(clean());
-  }
-  else {
-    done();
-  }
-});
-
-gulp.task('buildChartJs', () => {
-  return gulp.src('node_modules/chart.js/**/*.js').pipe(gulp.dest('static/node_modules/chart.js/'));
+gulp.task('createChartJs', () => {
+  return gulp.src('node_modules/chart.js/**/*.js')
+    .pipe(buster.resources())
+    .pipe(buster.references())
+    .pipe(gulp.dest('static/node_modules/chart.js/'));
 })
-
-gulp.task('createNodePackageJs', gulp.series('buildChartJs'));
 
 gulp.task('createJsLibs', () => {
   return gulp.src('frontend/js/libraries/**/*.js')
@@ -62,6 +103,8 @@ gulp.task('createJsLibs', () => {
         'min': '.js'
       }
     }))
+    .pipe(buster.resources())
+    .pipe(buster.references())
     .pipe(gulp.dest('static/js/libraries'));
 });
 
@@ -69,18 +112,19 @@ gulp.task('createJsScripts', () => {
   return gulp.src('frontend/js/scripts/**/*.js')
     .pipe(concat('scripts.js'))
     .pipe(minify())
+    .pipe(buster.resources())
+    .pipe(buster.references())
     .pipe(gulp.dest('static/js/scripts'));
 });
 
-gulp.task('buildJs', gulp.series('removeJs', 'createJsLibs', 'createJsScripts'));
-
-gulp.task('buildImg', () => {
+gulp.task('createImgs', () => {
   return gulp.src('frontend/img/**/*')
+    .pipe(buster.resources())
     .pipe(imagemin())
     .pipe(gulp.dest('static/img'));
 });
 
-gulp.task('buildFonts', () => {
+gulp.task('createFonts', () => {
   return gulp.src([
     'frontend/fonts/**/*.otf',
     'frontend/fonts/**/*.ttf',
@@ -88,28 +132,41 @@ gulp.task('buildFonts', () => {
     'frontend/fonts/**/*.eot',
     'frontend/fonts/**/*.woff',
     'frontend/fonts/**/*.woff2',
+  ])
+  .pipe(buster.resources())
+  .pipe(gulp.dest('static/fonts'));
+});
+
+gulp.task('buildFontCss', () => {
+  return gulp.src([
     'frontend/fonts/**/*.css', // Automatic generation of css by some font services. Makes things convenient.
   ])
+  .pipe(buster.resources())
+  .pipe(buster.references())
   .pipe(gulp.dest('static/fonts'));
-})
+});
+
+gulp.task('buildJs', gulp.series('removeJs', 'removeNodePackageJs', 'createChartJs', 'createJsLibs', 'createJsScripts'));
+gulp.task('buildSass', gulp.series('removeCss', 'createSass'));
+gulp.task('buildImg', gulp.series('removeImgs', 'createImgs'));
+gulp.task('buildFonts', gulp.series('removeFonts', 'createFonts', 'buildFontCss'));
 
 gulp.task('watch', () => {
-  gulp.watch('frontend/sass/**/*.scss', gulp.series('buildSass'));
-  gulp.watch(['node_modules/**/*.js'], gulp.series('removeNodePackageJs', 'createNodePackageJs'));
-  gulp.watch('frontend/js/**/*.js', gulp.series('removeJs', 'buildJs'));
-  gulp.watch('frontend/img/**/*', gulp.series('buildImg'));
-  gulp.watch('frontend/fonts/**/*', gulp.series('buildFonts'));
+  gulp.watch(['node_modules/**/*.js'], gulp.series('gatherFileHashes', 'buildJs'));
+  gulp.watch('frontend/js/**/*.js', gulp.series('gatherFileHashes', 'buildJs'));
+  gulp.watch('frontend/img/**/*', gulp.series('gatherFileHashes', 'buildImg', 'buildSass'));
+  gulp.watch('frontend/fonts/**/*', gulp.series('gatherFileHashes', 'buildFonts', 'buildSass'));
+  gulp.watch('frontend/sass/**/*.scss', gulp.series('gatherFileHashes', 'buildSass'));
 });
 
 gulp.task('build',
   gulp.series(
-    gulp.parallel(
-      'buildSass',
-      gulp.series('removeNodePackageJs','createNodePackageJs'),
-      gulp.series('removeJs', 'buildJs'),
-      'buildImg',
-      'buildFonts'
-    ), (done) => {
+    'gatherFileHashes',
+    'buildJs',
+    'buildImg',
+    'buildFonts',
+    'buildSass',
+    (done) => {
       done();
     }
   )
---
+++ partisan/templatetags/pp_extras.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+from django import template
+from django_project.settings import BASE_DIR
+import os
+from glob import glob
+
+register = template.Library()
+
+def get_directory_files(dir):
+  found_files = []
+  for path, subdirs, files in os.walk(dir):
+    for name in files:
+      found_files.append(os.path.join(path, name))
+  return found_files
+
+def cachebust_url(resource_path):
+  cached_path = resource_path
+  static_files = get_directory_files(str(BASE_DIR) + '/partisan/static')
+  for static_file in static_files:
+    if os.path.isfile(static_file):
+      if static_file.find('___') != -1:
+        path_without_cache = static_file[0:static_file.find('___')] + static_file[static_file.find('.'):]
+        path_without_cache = path_without_cache.replace(str(BASE_DIR) + '/partisan/static', '')
+        if resource_path == path_without_cache:
+          cached_path = '/static/' + '/'.join(Path(resource_path[1:]).parts[:-1]) + '/' + Path(static_file).name
+          break
+  return cached_path
+
+register.filter(name='cachebust_url', filter_func=cachebust_url)
