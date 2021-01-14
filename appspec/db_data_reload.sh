6af23e47c4353d1af4f919d57a22144ef448347a
Angelo Porretta <angelo.porretta@utoronto.ca>
2021-01-14 16:40:19 -0500

    Troubleshooting 143 error that is terminating script.

--- appspec/db_data_reload.sh
+++ appspec/db_data_reload.sh
@@ -8,14 +8,14 @@ then
   dbname=$(aws secretsmanager get-secret-value --secret-id "$DB_PASS_SECRET_NAME" | python3 -c "import sys, json; print(json.loads(json.load(sys.stdin)['SecretString'])['dbname'])")
   dbuser=$(aws secretsmanager get-secret-value --secret-id "$DB_PASS_SECRET_NAME" | python3 -c "import sys, json; print(json.loads(json.load(sys.stdin)['SecretString'])['username'])")
   dbpass=$(aws secretsmanager get-secret-value --secret-id "$DB_PASS_SECRET_NAME" | python3 -c "import sys, json; print(json.loads(json.load(sys.stdin)['SecretString'])['password'])")
-  dbsynced=$(mysql -u "$dbuser" -p"$dbpass" -h "$dbhost" -e "SELECT count(*) as '' FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = '$dbname'")
-  if [ dbsynced = "0" ]
-  then
+  #dbsynced=$(mysql -u "$dbuser" -p"$dbpass" -h "$dbhost" -e "SELECT count(*) as '' FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = '$dbname'")
+  #if [ dbsynced = "0" ]
+  #then
     aws s3 cp s3://elasticbeanstalk-us-west-2-054058811484/DbStarters/db.sql ./db.sql
-    mysql -u "$dbuser" -p"$dbpass" -h "$dbhost" "$dbname" < db.sql
-    rm db.sql
-    touch dbsynced
-  fi
+  #  mysql -u "$dbuser" -p"$dbpass" -h "$dbhost" "$dbname" < db.sql
+  #  rm db.sql
+  #  touch dbsynced
+  #fi
 fi
 cd /var/app/current
-python3 manage.py migrate
+#python3 manage.py migrate
