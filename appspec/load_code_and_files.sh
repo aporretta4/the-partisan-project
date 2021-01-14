1ba7bfa72008846f5a30cb91ae0b854a6a942e80
Angelo Porretta <angelo.porretta@utoronto.ca>
2021-01-14 15:28:22 -0500

    Need to install pip requirements before running migrations. Rearranged appspec steps.

--- appspec/load_code_and_files.sh
+++ appspec/load_code_and_files.sh
@@ -5,4 +5,3 @@ pkill gunicorn
 pip3 install -r /var/app/current/requirements.txt
 cd /var/app/current
 aws s3 sync "partisan/static/" "s3://$STATIC_FILE_BUCKET_NAME/static"
-/home/ec2-user/venv/bin/gunicorn --bind 127.0.0.1:8000 django_project.wsgi --daemon
--- appspec/webserver.sh
+++ appspec/webserver.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 source "/etc/profile.d/custom-envs.sh"
 . /home/ec2-user/venv/bin/activate
+pkill gunicorn
+/home/ec2-user/venv/bin/gunicorn --bind 127.0.0.1:8000 django_project.wsgi --daemon
 service httpd restart
