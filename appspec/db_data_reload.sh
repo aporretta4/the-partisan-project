71e8d131195a300709685fed18fa6cf63295d25c
Angelo Porretta <angelo.porretta@utoronto.ca>
2021-01-15 16:17:52 -0500

    Getting rid of "Already Synced" detection. Revisit at a different time.

--- appspec/db_data_reload.sh
+++ appspec/db_data_reload.sh
@@ -8,14 +8,10 @@ then
   dbname=$(aws secretsmanager get-secret-value --secret-id "$DB_PASS_SECRET_NAME" | python3 -c "import sys, json; print(json.loads(json.load(sys.stdin)['SecretString'])['dbname'])")
   dbuser=$(aws secretsmanager get-secret-value --secret-id "$DB_PASS_SECRET_NAME" | python3 -c "import sys, json; print(json.loads(json.load(sys.stdin)['SecretString'])['username'])")
   dbpass=$(aws secretsmanager get-secret-value --secret-id "$DB_PASS_SECRET_NAME" | python3 -c "import sys, json; print(json.loads(json.load(sys.stdin)['SecretString'])['password'])")
-  dbsynced=$(mysql -u "$dbuser" -p"$dbpass" -h "$dbhost" -e "SELECT count(*) as '' FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = '$dbname' AND TABLE_NAME = 'createdflag'")
-  if [ dbsynced = "0" ]
-  then
-    aws s3 cp s3://elasticbeanstalk-us-west-2-054058811484/DbStarters/db.sql ./db.sql
-    mysql -u "$dbuser" -p"$dbpass" -h "$dbhost" "$dbname" < db.sql
-    rm db.sql
-    mysql -u "$dbuser" -p"$dbpass" -h "$dbhost" -e "use $dbname; CREATE TABLE createdflag(flag int);"
-  fi
+  aws s3 cp s3://elasticbeanstalk-us-west-2-054058811484/DbStarters/db.sql ./db.sql
+  mysql -u "$dbuser" -p"$dbpass" -h "$dbhost" "$dbname" < db.sql
+  rm db.sql
+  mysql -u "$dbuser" -p"$dbpass" -h "$dbhost" -e "use $dbname; CREATE TABLE createdflag(flag int);"
 fi
 cd /var/app/current
 python3 manage.py migrate
