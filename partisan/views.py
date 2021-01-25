04ba8999d0014a4f0e9d1f46fbf071c5f1f6af2b
Angelo Porretta <angelo.porretta@utoronto.ca>
2021-01-25 14:20:10 -0500

    Adding reddit example cards and additional refactoring.

--- partisan/views.py
+++ partisan/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-from partisan.models import tweet, pie_chart_sentiment_stat
+from partisan.models import tweet, pie_chart_sentiment_stat, reddit_comment
 from partisan.p_classes.util.views_util import sentiment_retriever
 
 def index(request):
@@ -12,21 +12,24 @@ def index(request):
     'uk_politics': {},
     'covid': {}
   }
-  context['election']['examples'] = sentiment_retriever(sentimentable_model=tweet).getFullExampleDict(search_term='election', example_count=10)
+  context['election']['tw_examples'] = sentiment_retriever(sentimentable_model=tweet).getFullExampleDict(search_term='election', example_count=10)
   context['election']['sentiment_pie'] = sentiment_retriever.getSentimentPieChartDict('election')
-  context['us_politics']['examples'] = sentiment_retriever(sentimentable_model=tweet).getFullExampleDict(search_term='us politics', example_count=10)
+  context['us_politics']['tw_examples'] = sentiment_retriever(sentimentable_model=tweet).getFullExampleDict(search_term='us politics', example_count=10)
+  context['us_politics']['re_examples'] = sentiment_retriever(sentimentable_model=reddit_comment).getFullExampleDict(search_term='r/politics', example_count=10)
   context['us_politics']['sentiment_pie'] = sentiment_retriever.getSentimentPieChartDict('r/politics', 'us politics')
   context['us_politics']['sm_comparison'] = sentiment_retriever.getSentimentSocialMediaComparisonDict(
     pie_chart_sentiment_stat.objects.get(term__term='r/politics'),
     pie_chart_sentiment_stat.objects.filter(term__term='us politics', data_source='tw')[0]
   )
-  context['uk_politics']['examples'] = sentiment_retriever(sentimentable_model=tweet).getFullExampleDict(search_term='uk politics', example_count=10)
+  context['uk_politics']['tw_examples'] = sentiment_retriever(sentimentable_model=tweet).getFullExampleDict(search_term='uk politics', example_count=10)
+  context['uk_politics']['re_examples'] = sentiment_retriever(sentimentable_model=reddit_comment).getFullExampleDict(search_term='r/ukpolitics', example_count=10)
   context['uk_politics']['sentiment_pie'] = sentiment_retriever.getSentimentPieChartDict('r/ukpolitics', 'uk politics')
   context['uk_politics']['sm_comparison'] = sentiment_retriever.getSentimentSocialMediaComparisonDict(
     pie_chart_sentiment_stat.objects.get(term__term='r/ukpolitics'),
     pie_chart_sentiment_stat.objects.filter(term__term='uk politics', data_source='tw')[0]
   )
-  context['covid']['examples'] = sentiment_retriever(sentimentable_model=tweet).getFullExampleDict(search_term='covid', example_count=10)
+  context['covid']['tw_examples'] = sentiment_retriever(sentimentable_model=tweet).getFullExampleDict(search_term='covid', example_count=10)
+  context['covid']['re_examples'] = sentiment_retriever(sentimentable_model=reddit_comment).getFullExampleDict(search_term='r/Coronavirus', example_count=10)
   context['covid']['sentiment_pie'] = sentiment_retriever.getSentimentPieChartDict('r/Coronavirus', 'covid')
   context['covid']['sm_comparison'] = sentiment_retriever.getSentimentSocialMediaComparisonDict(
     pie_chart_sentiment_stat.objects.get(term__term='r/Coronavirus'),
