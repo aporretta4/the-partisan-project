bf7123271650aa1c2e896560057f014bb8554d43
Angelo Porretta <angelo.porretta@utoronto.ca>
2021-01-27 11:03:22 -0500

    Removing unnecessary db created flag and just dropping and recreating db.

--- appspec/db_data_reload.sh
+++ appspec/db_data_reload.sh
@@ -9,9 +9,10 @@ then
   dbuser=$(aws secretsmanager get-secret-value --secret-id "$DB_PASS_SECRET_NAME" | python3 -c "import sys, json; print(json.loads(json.load(sys.stdin)['SecretString'])['username'])")
   dbpass=$(aws secretsmanager get-secret-value --secret-id "$DB_PASS_SECRET_NAME" | python3 -c "import sys, json; print(json.loads(json.load(sys.stdin)['SecretString'])['password'])")
   aws s3 cp s3://elasticbeanstalk-us-west-2-054058811484/DbStarters/db.sql ./db.sql
+  mysql -u "$dbuser" -p"$dbpass" -h "$dbhost" -e "DROP DATABASE $dbname;"
+  mysql -u "$dbuser" -p"$dbpass" -h "$dbhost" -e "CREATE DATABASE $dbname;"
   mysql -u "$dbuser" -p"$dbpass" -h "$dbhost" "$dbname" < db.sql
   rm db.sql
-  mysql -u "$dbuser" -p"$dbpass" -h "$dbhost" -e "use $dbname; CREATE TABLE createdflag(flag int);"
 fi
 cd /var/app/current
 python3 manage.py migrate
