171cd081e41fc4c1559f9495ed1660eb1be3951a
Angelo Porretta <angelo.porretta@utoronto.ca>
2021-01-27 12:30:07 -0500

    Bad idea dropping and re-creating db. Admin isn't the root user so can't grant proper privileges.

--- appspec/db_data_reload.sh
+++ appspec/db_data_reload.sh
@@ -9,8 +9,6 @@ then
   dbuser=$(aws secretsmanager get-secret-value --secret-id "$DB_PASS_SECRET_NAME" | python3 -c "import sys, json; print(json.loads(json.load(sys.stdin)['SecretString'])['username'])")
   dbpass=$(aws secretsmanager get-secret-value --secret-id "$DB_PASS_SECRET_NAME" | python3 -c "import sys, json; print(json.loads(json.load(sys.stdin)['SecretString'])['password'])")
   aws s3 cp s3://elasticbeanstalk-us-west-2-054058811484/DbStarters/db.sql ./db.sql
-  mysql -u "$dbuser" -p"$dbpass" -h "$dbhost" -e "DROP DATABASE $dbname;"
-  mysql -u "$dbuser" -p"$dbpass" -h "$dbhost" -e "CREATE DATABASE $dbname;"
   mysql -u "$dbuser" -p"$dbpass" -h "$dbhost" "$dbname" < db.sql
   rm db.sql
 fi
